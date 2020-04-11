87642b1ed3b2c7617e35b2fff0848b866ba4f5d9
swyyauyan <swyyauyan.de@gmail.com>
2020-04-11 22:35:29 +0200

    controller and service build

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,11 +1,17 @@
+var OrderCreationService = require('../service/orderCreationService');
+var orderCreationService = new OrderCreationService;
+
 exports.getOrders = function(req, res) {
+    orderCreationService.get();
     res.send('NOT IMPLEMENTED: Get Order book');
 };
 
 exports.getOrderById = function(req, res) {
+    orderCreationService.getOrderById();
     res.send('NOT IMPLEMENTED: Get Order by ID = ' + req.params.id);
 };
 
 exports.createOrder = function(req, res) {
+    orderCreationService.create();
     res.send('NOT IMPLEMENTED: Create order');
 };
---
+++ service/orderCreationService.js
@@ -0,0 +1,23 @@
+var _ = require('lodash');
+
+class OrderCreationService {
+    get(){
+        console.log('********');
+        console.log('NOT IMPLEMENTED: Get Order book');
+        console.log('********');
+    }
+
+    getOrderById(){
+        console.log('********');
+        console.log('NOT IMPLEMENTED: Get Order bookGet Order by ID');
+        console.log('********');
+    }
+
+    create(){
+        console.log('********');
+        console.log('NOT IMPLEMENTED: create Order');
+        console.log('********');
+    }
+}
+
+module.exports = OrderCreationService;
