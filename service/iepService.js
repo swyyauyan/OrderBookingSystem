0f68f356301277dd1331e46598fb19cbbeace47d
swyyauyan <swyyauyan.de@gmail.com>
2020-05-03 13:12:17 +0200

    with get trade result API

--- service/iepService.js
+++ service/iepService.js
@@ -27,17 +27,8 @@ class IepService {
     //"Pre-opening session - Order Input Period",
     // "Pre-opening session - Pre-order matching Period",
 
-    var possibleResult = [];
-    var possibleIep = await this.getPossibleIep();
-
-    for(const iep of possibleIep){
-        var result = await this.getPossibleIepResult(iep);
-        possibleResult.push(result);
-    }
-
+    var possibleResult = await this.getPossibleIepResults();
     res.send(possibleResult);
-
-
 
     //STEP 2: When set trading phrase to "Pre-opening session - Order matching Period"
     //Recalcurate the IEP possible table (IepPossibleResult) and store in database.
@@ -48,10 +39,17 @@ class IepService {
   }
 
   async getTradeResult(res) {
-    res.send("NOT IMPLEMENTED");
     //STEP 1: Re - calculate "Possible IEP posiible trade."
     //"Pre-opening session - Order Input Period",
     // "Pre-opening session - Pre-order matching Period",
+    var tradeResults = [];
+    var possibleResult = await this.getPossibleIepResults();
+
+    for(const result of possibleResult){
+        var trade = await this.getTradeTable(result);
+        tradeResults.push({'iep': result.iep, 'trade': trade});
+    }
+    res.send(tradeResults);
 
     //STEP 2: When set trading phrase to "Pre-opening session - Order matching Period"
     //Recalcurate the IEP possible trade (IepPossibleTrade) and store in database.
@@ -62,7 +60,58 @@ class IepService {
     // "Continuous trading"
   }
 
+  async getTradeTable(iepResult) {
+    var tradeResults = [];
+    var askList = iepResult.accAskList;
+    var bidList = iepResult.accBidList;
+
+    bidList.forEach((bid) => {
+      var bidQty = bid.qty;
+      console.log(bidQty);
+      for (var i = 0; i < askList.length; i++) {
+        if (bidQty > 0) {
+          if (bidQty >= askList[i].qty) {
+            bidQty = bidQty - askList[i].qty;
+            tradeResults.push({
+              buyId: bid.orderId,
+              TradeGenrated: iepResult.iep + "@" + askList[i].qty,
+              askId: askList[i].orderId,
+            });
+            askList[i].qty = 0;
+          } else {
+            askList[i].qty = askList[i].qty - bidQty;
+            tradeResults.push({
+              buyId: bid.orderId,
+              TradeGenrated: iepResult.iep + "@" + bidQty,
+              askId: askList[i].orderId,
+            });
+            bidQty = 0;
+          }
+        }
+
+        askList = _.remove(askList, function (ask) {
+          return ask.qty != 0;
+        });
+      }
+    });
+    return tradeResults;
+    //RETURN: buying id, trade genrated and asking id
+  }
+
+  async getPossibleIepResults() {
+    var possibleResult = [];
+    var possibleIep = await this.getPossibleIep();
+
+    for (const iep of possibleIep) {
+      var result = await this.getPossibleIepResult(iep);
+      possibleResult.push(result);
+    }
+
+    return possibleResult;
+  }
+
   async getPossibleIep() {
+    //RETURN: Possible iep value list.
     var bidOrders = await Order.find({ action: "BID", type: "LIMIT" }).sort({
       price: -1,
       createAt: 1,
@@ -88,7 +137,11 @@ class IepService {
       var lowerAskPrice = askOrders[0].price;
 
       var possibleIep = [];
-      for (var iep = lowerAskPrice; iep <= highestBidPrice; iep = iep + interval) {
+      for (
+        var iep = lowerAskPrice;
+        iep <= highestBidPrice;
+        iep = iep + interval
+      ) {
         possibleIep.push(iep);
         console.log("Possible iep = " + iep);
       }
@@ -96,56 +149,48 @@ class IepService {
     }
   }
 
-  async getPossibleIepResult(iep){
-
-    var bidOrders = await Order.find({ action: "BID"}).sort({
+  async getPossibleIepResult(iep) {
+    //INPUT: iep value , RETURN iep value's possible Iep result (acc Bid, acc Ask and matched shared)
+    var bidOrders = await Order.find({ action: "BID" }).sort({
       price: -1,
       createAt: 1,
     });
 
-    var askOrders = await Order.find({ action: "ASK"}).sort({
+    var askOrders = await Order.find({ action: "ASK" }).sort({
       price: 1,
       createAt: 1,
     });
 
     var accBidQty = 0;
+    var accBidList = [];
     var accAskQty = 0;
+    var accAskList = [];
 
     bidOrders.forEach((order) => {
       if (order.type == "MARKET" || order.price >= iep) {
         accBidQty += order.qty;
+        accBidList.push(order);
       }
     });
 
     askOrders.forEach((order) => {
       if (order.type == "MARKET" || order.price <= iep) {
         accAskQty += order.qty;
+        accAskList.push(order);
       }
     });
 
     var matchedShared = Math.min(accBidQty, accAskQty);
+
 
     return {
       iep: iep,
       accBidQty: accBidQty,
       accAskQty: accAskQty,
       matchedShared: matchedShared,
-      accBidList: await this.getRemainList(bidOrders, matchedShared),
-      accAskList: await this.getRemainList(askOrders, matchedShared),
+      accBidList: accBidList,
+      accAskList: accAskList,
     };
-  }
-
-  async getRemainList(orders, shared) {
-    var accList = [];
-    var remainedShared = shared;
-    orders.forEach((order) => {
-      if (remainedShared > 0) {
-        accList.push(order);
-        remainedShared = remainedShared - order.qty;
-      }
-    });
-
-    return accList;
   }
 
 }
